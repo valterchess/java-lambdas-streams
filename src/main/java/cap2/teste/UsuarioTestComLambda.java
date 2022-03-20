cb26446f9340daf18499bf5ec3168db6fafc66d8
valterchess <valterrekabano7@gmail.com>
2022-03-19 21:26:11 -0300

    aplicando de forma simplificada e compreendendo o uso básico

--- src/main/java/cap2/teste/UsuarioTestComLambda.java
+++ src/main/java/cap2/teste/UsuarioTestComLambda.java
@@ -2,10 +2,18 @@ package main.java.cap2.teste;
 
 import main.java.cap2.classes.Usuario;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class UsuarioTestComLambda {
     public static void main(String[] args) {
+        Usuario usuario1 = new Usuario("Paulo Silveira", 150);
+        Usuario usuario2 = new Usuario("Rodrigo Turini", 120);
+        Usuario usuario3 = new Usuario("Guilherme Silveira", 190);
+
+        List<Usuario> usuarios = Arrays.asList(usuario1, usuario2, usuario3);
+
         // Opção do Lambda mais verbosa
         Consumer<Usuario> mostrador = (Usuario u) -> {System.out.println(u.getNome());};
 
@@ -15,5 +23,10 @@ public class UsuarioTestComLambda {
         // Opção direta
         Consumer<Usuario> mostrador3 = usuario -> System.out.println(usuario.getNome());
 
+        // Aplicando com o usu de variável
+        usuarios.forEach(mostrador);
+
+        /*Aplicando sem a necessidade da construção de uma variável mostrador */
+        usuarios.forEach(user -> System.out.println(user.getNome()));
     }
 }
